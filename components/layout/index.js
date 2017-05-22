6c28d3a62ea10469ccc2cfa55b282fb3c96a18bf
Perlou <a87387875@163.com>
2017-05-22 23:07:08 +0800

    feat global bg

---
+++ components/layout/index.js
@@ -0,0 +1,12 @@
+/**
+ * 公共组件样式
+ * @author Perlou(perloukevin@gmail.com)
+ */
+
+'use strict'
+
+import Background from './background.vue'
+
+module.exports = {
+  Background
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,66 @@
+const path = require('path')
+// const webpack = require('webpack')
+// const apiConfig
+
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  cache: {
+    max: 20,
+    maxAge: 600000
+  },
+  loading: {
+    color: '#2196f3'
+  },
+  build: {
+    extend (webpackConfig) {
+      webpackConfig.resolve.alias['~utils'] = path.join(__dirname, 'utils')
+      webpackConfig.resolve.alias['~static'] = path.join(__dirname, 'static')
+      webpackConfig.resolve.alias['~filters'] = path.join(__dirname, 'filters')
+      webpackConfig.resolve.alias['~services'] = path.join(__dirname, 'services')
+      webpackConfig.module.rules.push({
+        test: /\.scss$/,
+        loader: 'vue-style-loader!css-loader!sass-loader'
+      })
+    },
+    vendor: [
+      'axios',
+      'howler',
+      'swiper',
+      'marked',
+      'gravatar',
+      'particles.js',
+      'clipboard',
+      'highlight.js'
+    ],
+    babel: {
+      presets: ['es2015', 'stage-2'],
+      plugins: [
+        'transform-async-to-generator',
+        'transform-runtime'
+      ],
+      comments: true
+    }
+  },
+
+  dev: (process.env.NODE_ENV !== 'production'),
+  // env: {
+  //   baseUrl:
+  // }
+
+  plugins: [
+    { src: '~plugins/particles.js', ssr: false }
+  ],
+
   head: {
-    title: 'starter',
+    title: 'Perlou.cn',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { 'http-equiv': 'cleartype', content: 'on' },
+      { name: 'author', content: 'perloukevin@gmail.com' },
+      { name: 'MobileOptimized', content: '320' },
+      { name: 'HandheldFriendly', content: 'True' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no' }
     ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
   }
 }
+
